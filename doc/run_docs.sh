50cef287cbc14c91d724766b8b2a75a7d31dd1d9
nikita-skobov <nskobov.websites@gmail.com>
2020-05-22 20:32:05 -0500

    makes run_docs script output man file and html file

--- doc/run_docs.sh
+++ doc/run_docs.sh
@@ -3,6 +3,14 @@
 # usage:
 # ./doc/run_docs.sh > git-split.txt
 # cat ./git-split.txt | groff -mandoc -Thtml > git-split.html
+output_man_file() {
+    source ./lib/constants.bsc
+    source ./doc/build_manpages.sh
+}
 
-source ./lib/constants.bsc
-source ./doc/build_manpages.sh
+man_file_text=$(output_man_file)
+
+echo "$man_file_text" > ./dist/git-split.1
+cat ./dist/git-split.1 | groff -mandoc -Thtml > ./dist/git-split.html
+gzip ./dist/git-split.1
+
