4b652a3293db7c55dd85bfcd039b6ce36c2ffc94
nikita-skobov <nskobov.websites@gmail.com>
2020-05-23 01:06:17 -0500

    adds repo_file custom section to docs

--- doc/run_docs.sh
+++ doc/run_docs.sh
@@ -30,6 +30,30 @@ output_man_file() {
         doc_custom_sections+=("$i")
     done
 
+    repo_file_custom_section=(
+        "About the repo file"
+        "I created these tools with the intention of defining repo_files that contain information on how to split out/in local repositories back and forth from remote repositories. A repo_file is just a shell script that contains some variables. It is sourced by commands in this repository, and the variables that it sources are used to do the splitting out/in."
+        ""
+        "The following is a list of all valid variables you can use in your repo file."
+        ""
+        "The format is: <variable_name> - (type) - [required|optional|conditional]"
+        ""
+        "If the requiredness is 'conditional', read the paragraph under that variable name for an explanation"
+        ""
+        ""
+        ""
+    )
+    for i in "${rfv_valid_variable_names[@]}"; do
+        local -n array="rfv_$i"
+        # get a reference to the array
+        # i: variable name
+        # array[0]: type, [1]: opt/reqd, [2]: doc string
+        repo_file_custom_section+=("\fI$i\fR \- (${array[0]}) \- ${array[1]}")
+        repo_file_custom_section+=("${array[2]}")
+    done
+
+    doc_custom_sections+=("repo_file_custom_section")
+
     source ./doc/build_manpages.sh
 }
 
