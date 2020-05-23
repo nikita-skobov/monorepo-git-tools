d144288d688ad4f37a81f0f899b224a712d2e094
nikita-skobov <nskobov.websites@gmail.com>
2020-05-22 21:02:35 -0500

    adds global options to man page

--- doc/run_docs.sh
+++ doc/run_docs.sh
@@ -9,6 +9,27 @@
 # or <browser> ./dist/git-split.html
 output_man_file() {
     source ./lib/constants.bsc
+
+    # prepend the arrays defined in constants
+    # because the man page is slightly
+    # different than the --help text
+    local temp_doc_global_options=("Global options:")
+    for i in "${doc_global_options[@]}"; do
+        temp_doc_global_options+=("$i")
+    done
+    doc_global_options=()
+    for i in "${temp_doc_global_options[@]}"; do
+        doc_global_options+=("$i")
+    done
+    local temp_doc_custom_sections=("doc_global_options")
+    for i in "${doc_custom_sections[@]}"; do
+        temp_doc_custom_sections+=("$i")
+    done
+    doc_custom_sections=()
+    for i in "${temp_doc_custom_sections[@]}"; do
+        doc_custom_sections+=("$i")
+    done
+
     source ./doc/build_manpages.sh
 }
 
