511219eb285a439eea4cdfaf15ac41cb1a721665
nikita-skobov <nskobov.websites@gmail.com>
2020-05-22 20:51:32 -0500

    improves run_doc with gzip -f, and usage

--- doc/run_docs.sh
+++ doc/run_docs.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # usage:
-# ./doc/run_docs.sh > git-split.txt
-# cat ./git-split.txt | groff -mandoc -Thtml > git-split.html
+# must be run from repository root.
+# ./doc/run_docs.sh
+
+# to view the output:
+# man ./dist/git-split.1.gz
+# or <browser> ./dist/git-split.html
 output_man_file() {
     source ./lib/constants.bsc
     source ./doc/build_manpages.sh
@@ -12,5 +16,4 @@ man_file_text=$(output_man_file)
 
 echo "$man_file_text" > ./dist/git-split.1
 cat ./dist/git-split.1 | groff -mandoc -Thtml > ./dist/git-split.html
-gzip ./dist/git-split.1
-
+gzip ./dist/git-split.1 -f
