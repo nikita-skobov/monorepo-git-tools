1fea25f962bd1e92f5d43d5e9210f4e92502887b
nikita-skobov <nskobov.websites@gmail.com>
2020-05-30 22:53:57 -0500

    moves tests to seperate directories

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-source_combine git-split.bsc > test/git-split
-source_combine git-topbase.bsc > test/git-topbase
-chmod +x test/git-topbase
-chmod +x test/git-split
+source_combine git-split.bsc > test/split/git-split
+source_combine git-topbase.bsc > test/topbase/git-topbase
+chmod +x test/split/git-split
+chmod +x test/topbase/git-topbase
 
 # prevent running tests that involve remote access:
 if [[ $1 == "-l" || $1 == "--local-only" ]]; then
-    mv test/end-to-end-remote.bats test/tmpe2e.txt
-    bats test/
-    mv test/tmpe2e.txt test/end-to-end-remote.bats
+    mv test/split/end-to-end-remote.bats test/tmpe2e.txt
+    bats test/split
+    mv test/tmpe2e.txt test/split/end-to-end-remote.bats
 else
-    bats test/
+    bats test/split
 fi
 
