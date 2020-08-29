2d3994a104b478606a49db8b72548141425c419c
nikita-skobov <nskobov.websites@gmail.com>
2020-08-28 23:02:20 -0500

    seperates execution logic into seperate functions

--- src/exec_helpers.rs
+++ src/exec_helpers.rs
@@ -1,7 +1,19 @@
 use std::process::Command;
-use std::process::Stdio;
+use std::{io::Error, process::Stdio};
+
+pub struct CommandOutput {
+    stdout: String,
+    status: i32,
+}
 
 pub fn executed_successfully(exe_and_args: &[&str]) -> bool {
+    match execute(exe_and_args) {
+        Err(_) => false,
+        Ok(cmd_output) => cmd_output.status == 0,
+    }
+}
+
+pub fn execute(exe_and_args: &[&str]) -> Result<CommandOutput, Error> {
     // at the very least must provide the executable name
     assert!(exe_and_args.len() >= 1);
 
@@ -14,7 +26,15 @@ pub fn executed_successfully(exe_and_args: &[&str]) -> bool {
     let output = proc.output();
 
     match output {
-        Err(_) => false,
-        Ok(out) => out.status.success()
+        Err(e) => Err(e),
+        Ok(out) => {
+            let output_str_cow = String::from_utf8_lossy(&out.stdout);
+            Ok(
+                CommandOutput {
+                    stdout: output_str_cow.into_owned(),
+                    status: out.status.code().unwrap_or(1),
+                }
+            )
+        }
     }
 }
