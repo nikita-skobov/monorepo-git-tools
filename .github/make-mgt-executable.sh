9d60b80070618ed11e319b38efeca234b6f664f4
nikita-skobov <nskobov.websites@gmail.com>
2020-09-04 23:39:07 -0500

    adds github pipeline and scripts

---
+++ .github/make-mgt-executable.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# if any command fails, we want the script to exit with error
+set -e
+
+echo "$@"
+
+if [[ "$@" == *"windows"* ]]; then
+  echo "running windows"
+  ls ./target/"$1"/release/
+  ls ./target/"$1"/release/mgt.exe
+  cp ./target/"$1"/release/mgt.exe /usr/bin/mgt
+else
+  echo "running linux"
+  sudo cp ./target/"$1"/release/mgt /usr/bin/mgt
+  chmod +x /usr/bin/mgt
+fi
+
+echo "does /usr/bin/mgt work?"
+/usr/bin/mgt -h
+
+echo "does mgt work?"
+mgt -h
