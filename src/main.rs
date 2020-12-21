710cbb0f8f37667bd46b2bb8bca04be9b37c4a76
nikita-skobov <nskobov.websites@gmail.com>
2020-12-20 23:51:34 -0600

    removes old commands module. main uses new cli module

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,16 @@
-use clap::{App, ArgMatches};
 use die::*;
 
-mod commands;
 mod repo_file;
 mod split_out;
 mod split_in;
 mod topbase;
 mod check;
-mod split;
 mod exec_helpers;
 mod git_helpers3;
-
-fn get_cli_input<'a>() -> ArgMatches<'a> {
-    let version_str = format!(
-        "{} {}",
-        env!("CARGO_PKG_VERSION"),
-        env!("LATEST_COMMIT"),
-    );
-    let mut base_app = App::new(env!("CARGO_PKG_NAME"))
-        .version(version_str.as_str())
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"));
-
-    base_app = base_app.subcommands(vec![
-        commands::split_in(),
-        commands::split_in_as(),
-        commands::split_out(),
-        commands::split_out_as(),
-        commands::topbase(),
-        commands::check(),
-    ]);
-
-    return base_app.get_matches();
-}
+mod cli;
+mod core;
 
 fn main() {
-    let matches = get_cli_input();
-
-    if let Some(command_name) = matches.subcommand_name() {
-        commands::run_command(command_name, &matches);
-    }
+    let mgt = cli::get_cli_input();
+    cli::validate_input_and_run(mgt);
 }
