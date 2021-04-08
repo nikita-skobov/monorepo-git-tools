ef2a3c3d4a371ce43f0ec78376f25a9851aa5372
nikita-skobov <nskobov.websites@gmail.com>
2021-04-07 23:17:30 -0500

    uses gitfilter for split-in command, removes tests

--- src/split_in.rs
+++ src/split_in.rs
@@ -13,6 +13,7 @@ use super::die;
 use super::repo_file;
 use super::cli::MgtCommandSplit;
 use super::core;
+use super::verify;
 use super::topbase;
 
 
@@ -323,19 +324,13 @@ pub fn run_split_in_from_repo_file(
         cmd.dry_run
     );
 
-    let arg_strings = generate_arg_strings(&repo_file, cmd.dry_run, cmd.verbose);
-
     let log_p = if cmd.dry_run { "   # " } else { "" };
     if let Some(ref b) = cmd.output_branch {
         println!("{}Running filter commands on temporary branch: {}", log_p, b);
     }
-
-    arg_strings.filter_in(
-        &cmd.output_branch,
-        cmd.dry_run,
-        cmd.verbose
-    );
-
+    
+    let filter_rules = generate_gitfilter_filterrules(&repo_file, cmd.verbose);
+    core::perform_gitfilter(filter_rules, orphan_branch_name.clone(), cmd.dry_run, cmd.verbose);
     let res = if cmd.topbase.is_some() {
         println!("{}Topbasing", log_p);
         let should_add_branch_label = false;
@@ -371,6 +366,15 @@ pub fn run_split_in_from_repo_file(
     }
 
     println!("{}Success!", log_p);
+}
+
+fn generate_gitfilter_filterrules(
+    repo_file: &RepoFile,
+    verbose: bool,
+) -> gitfilter::filter::FilterRules {
+    let mut file_ops = verify::get_vec_of_file_ops_with_order(&repo_file, false);
+    let filter_rules = verify::make_filter_rules(&mut file_ops);
+    filter_rules
 }
 
 pub fn generate_repo_file(
@@ -437,226 +441,4 @@ fn validate_repo_file(
 
     core::panic_if_array_invalid(&repo_file.include, true, "include");
     core::panic_if_array_invalid(&repo_file.include_as, false, "include_as");
-}
-
-fn generate_arg_strings(
-    repo_file: &RepoFile,
-    dry_run: bool,
-    verbose: bool,
-) -> core::ArgStrings {
-    let log_p = if dry_run { " #  " } else { "" };
-    let include_as_arg_str = generate_split_out_arg_include_as(repo_file);
-    let exclude_arg_str = generate_split_out_arg_exclude(repo_file);
-    let include_arg_str = generate_split_out_arg_include(repo_file);
-
-    if verbose {
-        println!("{}include_arg_str: {}", log_p, include_arg_str.join(" "));
-        println!("{}include_as_arg_str: {}", log_p, include_as_arg_str.join(" "));
-        println!("{}exclude_arg_str: {}", log_p, exclude_arg_str.join(" "));
-    }
-
-    let include = if include_arg_str.len() != 0 {
-        Some(include_arg_str)
-    } else { None };
-
-    let include_as = if include_as_arg_str.len() != 0 {
-        Some(include_as_arg_str)
-    } else { None };
-
-    let exclude = if exclude_arg_str.len() != 0 {
-        Some(exclude_arg_str)
-    } else { None };
-
-    core::ArgStrings {
-        include_as,
-        include,
-        exclude,
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::collections::HashSet;
-
-    // this test assumes theres a target/ directory with rust builds
-    // this is gitignored, so shouldnt show up on git ls-files
-    #[test]
-    fn get_commited_paths_should_not_contain_ignored_files() {
-        let files = get_commited_paths();
-        assert!(files.contains(&PathBuf::from("Cargo.toml")));
-        assert!(files.contains(&PathBuf::from("src/split_in.rs")));
-        assert!(!files.contains(&PathBuf::from("target/release/mgt")));
-    }
-    #[test]
-    fn get_commited_paths_should_contain_the_folder_leading_to_the_file() {
-        let files = get_commited_paths();
-        assert!(files.contains(&PathBuf::from("test/")));
-        assert!(files.contains(&PathBuf::from("test/general/")));
-        assert!(files.contains(&PathBuf::from("test/general/end-to-end.bats")));
-    }
-
-    // this test requires reading files/folders from the root of the
-    // repo. make sure when you run tests, you are at the root of the repo
-    #[test]
-    fn generate_split_arg_include_as_should_return_the_mapping_as_is_if_src_is_file() {
-        let include_as = vec![
-            "src/lib/Cargo.toml", "Cargo.toml",
-        ];
-        let pathbufs = vec![
-            PathBuf::from("Cargo.toml")
-        ];
-        let s = generate_split_arg_include_as(&include_as, pathbufs);
-
-        let expected = format!("--path-rename {}:{}", include_as[1], include_as[0]);
-        assert_eq!(s.join(" "), expected);
-    }
-
-    // this test requires reading files/folders from the root of the
-    // repo. make sure when you run tests, you are at the root of the repo
-    #[test]
-    fn generate_split_arg_include_as_should_iterate_over_files_in_folder() {
-        let include_as = vec![
-            "sometestlib/", "test/general/",
-        ];
-        let pathbufs = vec![
-            PathBuf::from("test/general/end-to-end.bats"),
-            PathBuf::from("test/general/usage.bats"),
-        ];
-        let s_vec = generate_split_arg_include_as(&include_as, pathbufs);
-        let s = s_vec.join(" ");
-
-        let expected1 = "--path-rename test/general/end-to-end.bats:sometestlib/end-to-end.bats";
-        let expected2 = "--path-rename test/general/usage.bats:sometestlib/usage.bats";
-        assert!(s.contains(expected1));
-        assert!(s.contains(expected2));
-    }
-
-    // this test requires reading files/folders from the root of the
-    // repo. make sure when you run tests, you are at the root of the repo
-    #[test]
-    fn generate_split_arg_include_as_should_work_for_nested_folders() {
-        let include_as = vec![
-            "sometestlib/", "test/",
-        ];
-        // there are a lot of paths, wont check for all of them
-        let pathbufs = vec![
-            PathBuf::from("test/general/"),
-            PathBuf::from("test/general/end-to-end.bats"),
-            PathBuf::from("test/general/usage.bats"),
-            PathBuf::from("test/README.md"),
-            PathBuf::from("test/splitout/end-to-end.bats"),
-            PathBuf::from("test/splitout/"),
-        ];
-        let s_vec = generate_split_arg_include_as(&include_as, pathbufs);
-        let s = s_vec.join(" ");
-        println!("S: {}", s);
-
-        let expected1 = "--path-rename test/general/end-to-end.bats:sometestlib/general/end-to-end.bats";
-        let expected2 = "--path-rename test/README.md:sometestlib/README.md";
-        let expected3 = "--path-rename test/splitout/end-to-end.bats:sometestlib/splitout/end-to-end.bats";
-        assert!(s.contains(expected1));
-        assert!(s.contains(expected2));
-        assert!(s.contains(expected3));
-    }
-
-    #[test]
-    fn generate_split_arg_include_as_should_work_for_root_rename() {
-        let include_as = vec![
-            "sometestlib/", " ",
-        ];
-        let pathbufs = vec![
-            PathBuf::from("."),
-            PathBuf::from("test/"),
-            PathBuf::from("test/general/"),
-            PathBuf::from("test/general/end-to-end.bats"),
-            PathBuf::from("test/general/usage.bats"),
-            PathBuf::from("test/README.md"),
-            PathBuf::from("test/splitout/"),
-            PathBuf::from("test/splitout/end-to-end.bats"),
-            PathBuf::from("Cargo.toml"),
-        ];
-        let s_vec = generate_split_arg_include_as(&include_as, pathbufs);
-        let s = s_vec.join(" ");
-
-        let expected1 = "--path-rename test/general/end-to-end.bats:sometestlib/test/general/end-to-end.bats";
-        let expected2 = "--path-rename test/README.md:sometestlib/test/README.md";
-        let expected3 = "--path-rename Cargo.toml:sometestlib/Cargo.toml";
-        // there are a lot of paths, wont check for all of them
-        assert!(s.contains(expected1));
-        assert!(s.contains(expected2));
-        assert!(s.contains(expected3));
-    }
-
-    #[test]
-    fn generate_split_arg_include_as_should_not_have_duplicates() {
-        let include_as = vec![
-            "sometestlib/", " ",
-            "sometestlib/somesrc/", "src/",
-        ];
-        // this ones better to test with the real thing:
-        let valid_files = get_commited_paths();
-        let paths = generate_split_arg_include_as(&include_as, valid_files);
-        println!("S: {}", paths.join(" "));
-        let mut unique_paths = HashSet::new();
-        // we go every other one because there are a lot of entries
-        // that are just --path-rename, and that would get filtered out by the hash set
-        // otherwise
-        for p in paths.iter().skip(1).step_by(2) {
-            assert!(!unique_paths.contains(&p));
-            unique_paths.insert(p);
-        }
-    }
-
-    #[test]
-    fn generate_split_arg_include_as_should_not_contain_gitignored_files() {
-        let include_as = vec![
-            "sometestlib/", " ",
-        ];
-        let pathbufs = vec![
-            // pretend that only Cargo.toml is not gitignored
-            PathBuf::from("Cargo.toml"),
-        ];
-        let s_vec = generate_split_arg_include_as(&include_as, pathbufs);
-        let s = s_vec.join(" ");
-        println!("S: {}", s);
-
-        let expected = "--path-rename Cargo.toml:sometestlib/Cargo.toml";
-        let notexpected = "src/";
-
-        // since src/ is a directory that is "ignored",
-        // it shouldnt show up in the include_as_arg_str
-        assert!(!s.contains(notexpected));
-        assert!(s.contains(expected));
-    }
-
-    // // TODO: add this functionality. kinda annoying since it would need
-    // // to exist across several methods...
-    // //
-    // // we cant do this for all include_as since it can be files or folders
-    // // but in the case where user is bringing entire repo into a subdir
-    // // (ie: include_as=("subdir" " ") we can detect this case
-    // // so if the user forgets to place a trailing slash (which
-    // // it needs, otherwise filter-repo will not work), then we should
-    // // add one for them
-    // #[test]
-    // fn should_append_trailing_slash_if_missing_for_entire_repo_case() {
-    //     let matches = ArgMatches::new();
-    //     let mut runner = Runner::new(&matches);
-    //     // ensure we dont actually run anything
-    //     runner.dry_run = true;
-    //     let mut repofile = RepoFile::new();
-    //     // include_as has dest:src for split in
-    //     // because its the reverse of the split out
-    //     repofile.include_as = Some(vec![
-    //         "locallib".into(), " ".into(),
-    //     ]);
-    //     runner.repo_file = repofile;
-    //     runner = runner.generate_arg_strings();
-    //     let include_arg_str_opt = runner.include_arg_str.clone();
-    //     let include_as_arg_str_opt = runner.include_as_arg_str.clone();
-
-    //     assert_eq!(include_as_arg_str_opt.unwrap(), "--path-rename  :locallib/");
-    //     assert_eq!(include_arg_str_opt.unwrap(), "--path locallib/");
-    // }
 }
